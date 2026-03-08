0867b992e3d55ec42779b4df5fd6a3c34a116728
PedroRosalba <pedro.dandrea.rpd@gmail.com>
2026-03-07 22:28:33 -0300

    chore: add rpc url config

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,9 @@
-export const DEFAULT_SEPOLIA_RPC_URL = 'https://starknet-sepolia.g.alchemy.com/starknet/version/rpc/v0_10/dql5pMT88iueZWl7L0yzT56uVk0EBU4L';
 export const STRK_TOKEN_ADDRESS ='0x04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d';
+
+export const SEPOLIA_CONFIG = {
+    registryAddress: '0x30e391e0fb3020ccdf4d087ef3b9ac43dae293fe77c96897ced8cc86a92c1f0',
+    factoryAddress: '0x2175848fdac537a13a84aa16b5c1d7cdd4ea063cd7ed344266b99ccc4395085',
+    accountClassHash: '0x30d37d3acccb722a61acb177f6a5c197adb26c6ef09cb9ba55d426ebf07a427',
+    rpcUrl: 'https://starknet-sepolia.g.alchemy.com/starknet/version/rpc/v0_10/dql5pMT88iueZWl7L0yzT56uVk0EBU4L',
+    chainId: '0x534e5f5345504f4c4941',
+  };
